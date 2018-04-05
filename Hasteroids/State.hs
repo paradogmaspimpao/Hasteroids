e29b0bab5f73890b216ad508b139732da8a0d1df
Samuel Borges <samuelmordred@gmail.com>
2018-04-04 23:27:06 -0300

    Rotacao da nave

--- Hasteroids/State.hs
+++ Hasteroids/State.hs
@@ -2,6 +2,7 @@ module Hasteroids.State (GameState(..), initialGameState,) where
 
 import Hasteroids.Player
 import Hasteroids.Geometry
+import Hasteroids.Geometry.Transform
 import Hasteroids.Render (LineRenderable(..))
 
 data GameState = GameState { statePlayer :: Player }
@@ -15,7 +16,7 @@ initialGameState = GameState {
     }
 
 initialPlayerState :: Player
-initialPlayerState = Player (400, 300)
+initialPlayerState = Player $ Body {bodyPos=(400, 300), bodyAngle=pi/4.0}
 
 stateLines :: GameState -> [LineSegment]
 stateLines = lineSegments . statePlayer
